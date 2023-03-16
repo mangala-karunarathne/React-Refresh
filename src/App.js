497901d003730939730b2c9215bde763808b2d21
mangala-karunarathne <mangalaicc@gmail.com>
2023-03-16 06:29:37 +0530

    CHG | In Line Styles with turnary Operators

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 function App() {
 
   const age = 10;
+  const isGreen = true;
 
   return (
     <div className="App">
     {age>= 18 ? <h1>Over Age</h1> : <h1> Under Age</h1>}
+    <h1 style={{color: isGreen ? 'green' : "red"}}>Age Color</h1>
     </div>
   );
 }
