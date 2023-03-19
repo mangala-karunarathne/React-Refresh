7805aaaaac3405c50c4621467255360361deed51
mangala-karunarathne <mangalaicc@gmail.com>
2023-03-19 10:36:19 +0530

    CHG | useState count Excercise

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [textColor, setTextColor] = useState("green");
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count+1)
+  };
+  const decreaseCount = () => {
+    setCount(count-1)
+  };
+  const zeroSet = () => {
+    setCount(0)
+  };
 
   return (
     <div className="App">
-      <button onClick={()=>{
-        setTextColor(textColor === "green" ? "red" : "green")
-      }}> Green / Red </button>
-      <h1 style={{color: textColor}}> Here is MK Dev</h1>
+        <button onClick={increaseCount}>Increase</button>
+        <button onClick={decreaseCount}>Decrese</button>
+        <button onClick={zeroSet}>Set to Zero</button>
+        <div>
+          {count}
+        </div>
     </div>
   );
 }
