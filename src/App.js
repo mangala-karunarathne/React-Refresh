299dfa7e24ae0e4866f4c51b36d7a8770651d222
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-05 16:25:34 +0530

    CHG | Get Data From API using Fetch Mtd

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { useState } from "react";
 import Text from "./Text";
 
 function App() {
-  const [showText, setShowText] = useState(false);
-
+  fetch("https://catfact.ninja/fact")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <div className="App">
-      <button onClick={()=>{
-        setShowText(!showText)
-      }}>
-        Show Text
-      </button>
-    {showText && <Text setShowText={setShowText}/>}
+      <button>Generate Cat Fact</button>
+      <p> </p>
     </div>
   );
 }
