0c96fb186ced252b2b8657a892d9c011a24fcd15
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-05 17:07:16 +0530

    CHG | Getting Data with on Click

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Text from "./Text";
 import Axios from "axios";
 
 function App() {
   const [catFact, setCatFact] = useState("");
 
-  Axios.get("https://catfact.ninja/fact").then((res) => {
-    console.log(res.data);
-    setCatFact(res.data.fact);
-  });
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      console.log(res.data);
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
 
   return (
     <div className="App">
-      <button>Generate Cat Fact</button>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
       <p>{catFact}</p>
     </div>
   );
