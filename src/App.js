fad1f1d52ce076e42f95ebcb243a3a1789203174
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-05 16:32:22 +0530

    CHG | Get Data From API using Axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { useState } from "react";
 import Text from "./Text";
+import Axios from "axios";
 
 function App() {
-  fetch("https://catfact.ninja/fact")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+      console.log(res.data);
+    })
+
   return (
     <div className="App">
       <button>Generate Cat Fact</button>
