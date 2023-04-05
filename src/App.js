8e179b91781c1d7c893e0f9bf80be99f270db830
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-05 19:50:58 +0530

    CHG | Getting Age using API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import Text from "./Text";
 import Axios from "axios";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
 
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      console.log(res.data);
-      setCatFact(res.data.fact);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      console.log(res.data.age);
+      setAge(res.data.age);
     });
   };
 
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
-
   return (
     <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input
+        placeholder="Ex. Mangala.."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>Predicted Age: {age} </h1>
+      
     </div>
   );
 }
